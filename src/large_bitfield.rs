3c0c0ffd939c1c9fcf5504f6bb65592c3ab32d2c
Raz <raz-@outlook.com>
2019-04-20 11:45:24 -0700

    Docs

--- src/large_bitfield.rs
+++ src/large_bitfield.rs
@@ -1,18 +1,28 @@
-//
-// Large Bitfield
-//
+//! # Large Bitfield
+//! `large_bitfield` defines the structure and fast_bitfield interface for Large Bitfieds.
+//! A Large Bitfield is a strcture that holds an array of `sizeof(usize) * 8` `usize` values as well
+//! as a "layer_cache" `usize` field to quickly determine highest and lowest set bits.
 
 use crate::{
     find_highest_set_bit, find_lowest_set_bit, FastBitField, LARGE_BIT_FIELD_BIT_SIZE,
     SMALL_BIT_FIELD_BIT_SIZE,
 };
 
+/// Defines the structure of the LargeBitField
 pub struct LargeBitField {
+    /// Holds a bitfield describing which sub bitfields currently have any set bits.
     layer_cache: usize,
+
+    /// Holds the bitfield state.
     bitfield: [usize; SMALL_BIT_FIELD_BIT_SIZE],
 }
 
+/// Defines the FastBitField interface for LargeBitField.
 impl LargeBitField {
+    /// Creates a new, empty LargeBitField
+    ///
+    /// # Returns
+    /// A LargeBitField.
     pub fn new() -> LargeBitField {
         LargeBitField {
             layer_cache: 0,
@@ -21,19 +31,20 @@ impl LargeBitField {
     }
 }
 
+/// Defines the FastBitField interface for LargeBitField.
 impl FastBitField for LargeBitField {
-    //
-    // Functions
-    //
-
+    /// Gets the number of bits available in the bitfield type.
+    ///
+    /// # Returns
+    /// The number of bits available.
     fn get_number_of_bits() -> usize {
         LARGE_BIT_FIELD_BIT_SIZE
     }
 
-    //
-    // Methods
-    //
-
+    /// Sets a bit in the bit field
+    ///
+    /// # Arguments
+    /// index - Provides the bit to set.
     fn set_bit(&mut self, index: usize) {
         if index >= LARGE_BIT_FIELD_BIT_SIZE {
             return;
@@ -44,12 +55,21 @@ impl FastBitField for LargeBitField {
 
         self.layer_cache |= 1 << top_layer;
 
+        //
+        // UNSAFE: top_layer is guaraneed to be between 0 and SMALL_BIT_FIELD_SIZE - 1.
+        // No need to perform bounds checking on the array.
+        //
+
         unsafe {
             let sub_field = self.bitfield.get_unchecked_mut(top_layer);
             *sub_field |= 1 << bottom_layer;
         }
     }
 
+    /// Clears a bit in the bit field
+    ///
+    /// # Arguments
+    /// index - Provides the bit to clear.
     fn clear_bit(&mut self, index: usize) {
         if index >= LARGE_BIT_FIELD_BIT_SIZE {
             return;
@@ -57,6 +77,11 @@ impl FastBitField for LargeBitField {
 
         let top_layer = index / SMALL_BIT_FIELD_BIT_SIZE;
         let bottom_layer = index % SMALL_BIT_FIELD_BIT_SIZE;
+
+        //
+        // UNSAFE: top_layer is guaraneed to be between 0 and SMALL_BIT_FIELD_SIZE - 1.
+        // No need to perform bounds checking on the array.
+        //
 
         unsafe {
             let sub_field = self.bitfield.get_unchecked_mut(top_layer);
@@ -68,6 +93,10 @@ impl FastBitField for LargeBitField {
         }
     }
 
+    /// Gets the lowest set bit.
+    ///
+    /// # Returns
+    /// The lowest set bit index or -1 if no bits are set.
     fn get_lowest_set_bit(&self) -> isize {
         if self.is_empty() {
             return -1;
@@ -76,6 +105,10 @@ impl FastBitField for LargeBitField {
         self.get_lowest_set_bit_unchecked() as isize
     }
 
+    /// Gets the highest set bit.
+    ///
+    /// # Returns
+    /// The highest set bit index or -1 if no bits are set.
     fn get_highest_set_bit(&self) -> isize {
         if self.is_empty() {
             return -1;
@@ -84,8 +117,21 @@ impl FastBitField for LargeBitField {
         self.get_highest_set_bit_unchecked() as isize
     }
 
+    /// Gets the lowest set bit, guaranteed to have no branches and be in constant time, completely
+    /// invariant of the state of the bit field. If no bits are set, the result is undefined.
+    ///
+    /// This function should only be used if the caller can guarantee the bitfield will always
+    /// have at least one bit set.
+    ///
+    /// # Returns
+    /// The lowest set bit index or UNDEFINED if no bits are set.
     fn get_lowest_set_bit_unchecked(&self) -> usize {
         let level = find_lowest_set_bit(self.layer_cache);
+
+        //
+        // UNSAFE: level is guaranteed to be between 0 and SMALL_BIT_FIELD_SIZE - 1 by the
+        // the definition of find_lowest_set_bit. No need to perform bounds checking on the array.
+        //
 
         unsafe {
             let sub_field = self.bitfield.get_unchecked(level);
@@ -93,8 +139,21 @@ impl FastBitField for LargeBitField {
         }
     }
 
+    /// Gets the highest set bit, guaranteed to have no branches and be in constant time, completely
+    /// invariant of the state of the bit field. If no bits are set, the result is undefined.
+    ///
+    /// This function should only be used if the caller can guarantee the bitfield will always
+    /// have at least one bit set.
+    ///
+    /// # Returns
+    /// The highest set bit index or UNDEFINED if no bits are set.
     fn get_highest_set_bit_unchecked(&self) -> usize {
         let level = find_highest_set_bit(self.layer_cache);
+
+        //
+        // UNSAFE: level is guaranteed to be between 0 and SMALL_BIT_FIELD_SIZE - 1 by the
+        // the definition of find_highest_set_bit. No need to perform bounds checking on the array.
+        //
 
         unsafe {
             let sub_field = self.bitfield.get_unchecked(level);
@@ -102,6 +161,10 @@ impl FastBitField for LargeBitField {
         }
     }
 
+    /// Determines whether or not the bitfield is empty.
+    ///
+    /// # Retuns
+    /// true if empty, false otherwise.
     fn is_empty(&self) -> bool {
         self.layer_cache == 0
     }
