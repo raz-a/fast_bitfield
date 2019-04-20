e862bbff9c82eb3caa26a2a9a1849f99ed5bbd4c
Raz <raz-@outlook.com>
2019-04-20 11:14:13 -0700

    Lib comments

--- src/lib.rs
+++ src/lib.rs
@@ -69,12 +69,23 @@ pub trait FastBitField {
     fn is_empty(&self) -> bool;
 }
 
-pub mod large_bit_field;
+/// Defines a fast bitfield that contains `sizeof(usize) * 8` bits.
 pub mod small_bit_field;
+
+/// Defines a fast bitfield that contains `sizeof(usize) * sizeof(usize) * 8` bits.
+pub mod large_bit_field;
+
 
 const SMALL_BIT_FIELD_BIT_SIZE: usize = core::mem::size_of::<usize>() * 8;
 const LARGE_BIT_FIELD_BIT_SIZE: usize = SMALL_BIT_FIELD_BIT_SIZE * SMALL_BIT_FIELD_BIT_SIZE;
 
+/// Gets the lowest set bit of a usize value.
+///
+/// # Arguments
+/// value - The value to find the lowest set bit for.
+///
+/// # Returns
+/// The lowest set bit index or UNDEFINED if no bits are set.
 fn find_lowest_set_bit(value: usize) -> usize {
     if cpu_features::opcodes::count_leading_zeros_exists() {
         value.trailing_zeros() as usize
@@ -83,6 +94,13 @@ fn find_lowest_set_bit(value: usize) -> usize {
     }
 }
 
+/// Gets the highest set bit of a usize value.
+///
+/// # Arguments
+/// value - The value to find the highest set bit for.
+///
+/// # Returns
+/// The highest set bit index or UNDEFINED if no bits are set.
 fn find_highest_set_bit(value: usize) -> usize {
     if cpu_features::opcodes::count_leading_zeros_exists() {
         SMALL_BIT_FIELD_BIT_SIZE - 1 - value.leading_zeros() as usize
